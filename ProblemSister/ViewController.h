6ee4358880bb0b94ff861542d0d0fb83c7f76625
13211147 <383974871@qq.com>
2016-10-01 22:39:01 +0800

    initialize app

---
+++ ProblemSister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProblemSister
+//
+//  Created by michael on 2016/10/1.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProblemSister/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  ProblemSister
+//
+//  Created by michael on 2016/10/1.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
