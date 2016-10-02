82725d89b11e7daba3e6867bacd673ff3fe36695
13211147 <383974871@qq.com>
2016-10-02 20:25:34 +0800

    add UIView+XMGExtension and PrefixHeader

---
+++ ProblemSister/UIView+XMGExtension.h
@@ -0,0 +1,20 @@
+//
+//  UIView+XMGExtension.h
+//  ProblemSister
+//
+//  Created by michael on 2016/10/2.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (XMGExtension)
+
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+
+/** 在分类中声明@property，只会生成方法的声明，不会生成方法的实现和带有_下划线的成员变量*/
+
+@end
