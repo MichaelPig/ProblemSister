3d7650d398d4a5d6e04745ee485eeb460665d775
13211147 <383974871@qq.com>
2016-10-16 11:25:10 +0800

    could exchange login and register

--- ProblemSister/Classes/FriendTrends-关注/Controller/XMGLoginRegisterViewController.m
+++ ProblemSister/Classes/FriendTrends-关注/Controller/XMGLoginRegisterViewController.m
@@ -10,20 +10,47 @@
 
 @interface XMGLoginRegisterViewController ()
 
+/** 登录框距离控制器view左边的间距 */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginViewLeftMargin;
+
 @end
 
 @implementation XMGLoginRegisterViewController
 
+
+- (IBAction)back {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)showLoginOrRegister:(UIButton *)sender {
+    
+    //退出键盘
+    [self.view endEditing:YES];
+    
+    if (self.loginViewLeftMargin.constant == 0) {//显示注册界面
+        self.loginViewLeftMargin.constant = - self.view.width;
+        sender.selected = YES;
+//        [sender setTitle:@"已有账号？" forState:UIControlStateNormal];
+    }else{//显示登录界面
+        self.loginViewLeftMargin.constant = 0;
+        sender.selected = NO;
+//        [sender setTitle:@"注册账号" forState:UIControlStateNormal];
+    }
+    
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
+
+
 
 /**
  * 让当前控制器对应的状态栏是白色
